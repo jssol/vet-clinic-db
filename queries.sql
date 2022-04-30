4c661dc183b6fae8bda3c5384191d74606cd6a2a
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-30 02:00:33 +0200

    Set all species to unspecified

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/*Queries that set all the species row to 'unspecified' and then rollback */
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
