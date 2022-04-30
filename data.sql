6bb4bf978efed95d1e4bf856151c32a0d62e4f61
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-30 15:05:16 +0200

    Insert owners

--- data.sql
+++ data.sql
@@ -11,3 +11,13 @@ INSERT INTO animals (name, date_of_birth,escape_attempts, neutered, weight_kg) V
 INSERT INTO animals (name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-07', 7, TRUE, 20.4);
 INSERT INTO animals (name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
 INSERT INTO animals (name, date_of_birth,escape_attempts, neutered, weight_kg) VALUES ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+/* Insert owners into the owners table */
+INSERT INTO owners (full_name, age) 
+VALUES ('Sam Smith', 34),
+	   ('Jennifer Orwell', 19),
+	   ('Bob', 45),
+	   ('Melody Pond', 77),
+	   ('Dean Winchester', 14),
+	   ('Jodie Whittaker', 38);
+
