8366db2beb580d70a20786948dc0bda713c3468f
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-30 21:56:54 +0200

    Insert specialities into the specialities table

--- data.sql
+++ data.sql
@@ -58,5 +58,12 @@ VALUES ('William Tatcher', 45, '2000-04-23'),
 			 ('Maisy Smith', 26, '2019-01-17'),
 			 ('Stephanie Mendez', 64, '1981-05-04'),
 			 ('Jack Harkness', 38, '2008-06-08');
+			 
+/* Insert specialities into the specialities table */
+INSERT INTO specializations (vet_id,  species_id) 
+VALUES (1, 1),
+			 (3, 2),
+			 (3, 1),
+			 (4, 2);
 	   
 
